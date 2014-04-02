8507027d48373598055d913f6547bc0670f29202
Shawn McGough <shawn.mcgough@gmail.com>
2014-04-02 19:24:40 -0400

    extensions for async sockets

--- papercut/papercut/Extensions.cs
+++ papercut/papercut/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
@@ -11,6 +12,20 @@ namespace papercut
         public static Task ConnectTaskAsync(this Socket socket, EndPoint endpoint)
         {
             return Task.Factory.FromAsync(socket.BeginConnect, socket.EndConnect, endpoint, null);
+        }
+
+        public static Task<int> SendTaskAsync(this Socket socket, byte[] buffer, int offset, int size, SocketFlags flags)
+        {
+            AsyncCallback nullOp = (i) => { };
+            IAsyncResult result = socket.BeginSend(buffer, offset, size, flags, nullOp, socket);
+            // Use overload that takes an IAsyncResult directly
+            return Task.Factory.FromAsync(result, (i) => socket.EndSend(i));
+        }
+        public static async Task WriteAsync(this Socket socket, byte[] buffer)
+        {
+            var bytesSent = 0;
+            while (bytesSent != buffer.Length)
+                bytesSent += await socket.SendTaskAsync(buffer, bytesSent, buffer.Length - bytesSent, SocketFlags.None);
         }
 
     }
